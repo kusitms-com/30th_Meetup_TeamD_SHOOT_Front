2ce377492842e39571e8c2314bd43e316436fc0a
yena0213 <yeana3131@naver.com>
2024-11-10 23:43:48 +0900

    [chore] 오류 해결:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/signup/SignupPage';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import LoginPage from './pages/LoginPage';
+// import SignupPage from './pages/signup/SignupPage';
 import MainPage from './pages/main/MainPage';
 
 
