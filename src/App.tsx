03e51ab16a64c046df0c1e61b7966a58e802842a
yena0213 <yeana3131@naver.com>
2024-11-11 16:33:57 +0900

    [Feat] 로그인 연결

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import MainPage from './pages/main/MainPage';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/signin" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/signin" element={<LoginPage />} />
+                <Route path="/signup" element={<SignupPage />} />
+            </Routes>
     );
 };
 
