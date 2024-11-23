be72af796c12e4dc0c86cdfa42a23e594b2d62cd
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-23 16:12:09 +0900

    fix: import userStore 수정

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,20 +1,21 @@
-// store/useUserStore.ts
 import { create } from "zustand";
+
+// 타입 지정
 
 interface UserStore {
   username: string;
   email: string;
   ImgUrl: string;
   userId: string;
-  setUser: (user: Partial<UserStore>) => void; 
 }
+
+// 초기값 지정
 
 const useUserStore = create<UserStore>()((set) => ({
   username: "",
-  email: "",
-  ImgUrl: "",
-  userId: "",
-  setUser: (user) => set((state) => ({ ...state, ...user })), 
+  email:"",
+  ImgUrl:"",
+  userId: ""
 }));
 
 export default useUserStore;
